d0f144128b494805f32460418d08d8e14f0ba585
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-01 22:08:56 +0530

    add counter logic

---
+++ public/index.js
@@ -0,0 +1,13 @@
+function incrementCounter() {
+  let value = document.getElementById("counter-value").innerText;
+  value = parseInt(value) + 1;
+  document.getElementById("counter-value").innerText = value;
+}
+
+function decrementCounter() {
+  let value = document.getElementById("counter-value").innerText;
+  if (value > 0) {
+    value = parseInt(value) - 1;
+    document.getElementById("counter-value").innerText = value;
+  }
+}
